8195e956bedb49012c1e02c651065f5640465672
Karel Minarik <karmi@karmi.cz>
2011-02-07 18:11:21 +0100

    Get rid of autotolading stuff and just require the files

--- lib/slingshot.rb
+++ lib/slingshot.rb
@@ -2,23 +2,15 @@ require 'rest_client'
 require 'yajl/json_gem'
 
 require 'slingshot/rubyext/hash'
+require 'slingshot/configuration'
+require 'slingshot/client'
+require 'slingshot/client'
+require 'slingshot/search'
+require 'slingshot/search/query'
+require 'slingshot/search/sort'
+require 'slingshot/search/facet'
+require 'slingshot/results/collection'
+require 'slingshot/index'
 
 module Slingshot
-
-  autoload :Configuration, File.dirname(__FILE__) + '/slingshot/configuration'
-  autoload :Client,        File.dirname(__FILE__) + '/slingshot/client'
-  autoload :RestClient,    File.dirname(__FILE__) + '/slingshot/client'
-
-  autoload :Search,        File.dirname(__FILE__) + '/slingshot/search'
-
-  module Search
-    autoload :Query,       File.dirname(__FILE__) + '/slingshot/search/query'
-    autoload :Sort,        File.dirname(__FILE__) + '/slingshot/search/sort'
-    autoload :Facet,       File.dirname(__FILE__) + '/slingshot/search/facet'
-  end
-  module Results
-    autoload :Collection,  File.dirname(__FILE__) + '/slingshot/results/collection'
-  end
-
-  autoload :Index,         File.dirname(__FILE__) + '/slingshot/index'
 end
