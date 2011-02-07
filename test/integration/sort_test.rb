7bd167b29eee8fffda243736c225fe6131ca9da6
Karel Minarik <karmi@karmi.cz>
2011-02-07 20:18:31 +0100

    Added basic integration tests for sorting

---
+++ test/integration/sort_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+module Slingshot
+
+  class SortIntegrationTest < Test::Unit::TestCase
+    include Test::Integration
+
+    context "Sort" do
+
+      should "sort by title" do
+        q = '*'
+        s = Slingshot.search('articles-test') do
+          query { string q }
+          sort  { title }
+        end
+
+        assert_equal 5, s.results.count
+        assert_equal 'Five', s.results.first['_source']['title']
+      end
+
+      should "sort by title, descending" do
+        q = '*'
+        s = Slingshot.search('articles-test') do
+          query { string q }
+          sort  { title :desc }
+        end
+
+        assert_equal 5, s.results.count
+        assert_equal 'Two', s.results.first['_source']['title']
+      end
+
+    end
+
+  end
+
+end
