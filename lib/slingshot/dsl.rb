8b58d08b5b43687833db6d433c47284bd27aff8c
Karel Minarik <karmi@karmi.cz>
2011-02-07 20:51:23 +0100

    Added DSL.configure helper method for better symmetry

--- lib/slingshot/dsl.rb
+++ lib/slingshot/dsl.rb
@@ -1,6 +1,10 @@
 module Slingshot
   module DSL
 
+    def configure(&block)
+      Configuration.class_eval(&block)
+    end
+
     def search(indices, &block)
       Search::Search.new(indices, &block).perform
     end
