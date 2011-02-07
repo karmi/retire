fee340f7ef4d86b2fe96f59fb008915cc79f97dc
Karel Minarik <karmi@karmi.cz>
2011-02-07 21:17:00 +0100

    Added full example using the DSL

---
+++ examples/dsl.rb
@@ -0,0 +1,70 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'rubygems'
+require 'slingshot'
+
+extend Slingshot::DSL
+
+configure do
+  url "http://localhost:9200"
+end
+
+index 'articles' do
+  delete
+  create
+
+  puts "Documents:", "-"*80
+  [
+    { :title => 'One',   :tags => ['ruby'] },
+    { :title => 'Two',   :tags => ['ruby', 'python'] },
+    { :title => 'Three', :tags => ['java'] },
+    { :title => 'Four',  :tags => ['ruby', 'php'] }
+  ].each do |article|
+    puts "Indexing article: #{article.to_json}"
+    store article
+  end  
+
+  refresh
+end
+
+s = search 'articles' do
+  query do
+    terms :tags, ['ruby']
+  end
+
+  sort do
+    title 'desc'
+  end
+
+  facet 'global-tags' do
+    terms :tags, :global => true
+  end
+
+  facet 'current-tags' do
+    terms :tags
+  end
+end
+
+puts "", "Query:", "-"*80
+puts s.to_json
+
+puts "", "Raw JSON result:", "-"*80
+puts JSON.pretty_generate(s.response)
+
+puts "", "Try the query in Curl:", "-"*80
+puts s.to_curl
+
+puts "", "Results:", "-"*80
+s.results.each_with_index do |document, i|
+  puts "#{i+1}. #{ document['_source']['title'].ljust(20) } [id] #{document['_id']}"
+end
+
+puts "", "Facets: tags distribution across the whole database:", "-"*80
+s.results.facets['global-tags']['terms'].each do |f|
+  puts "#{f['term'].ljust(10)} #{f['count']}"
+end
+
+puts "", "Facets: tags distribution for the current query", "-"*80
+s.results.facets['current-tags']['terms'].each do |f|
+  puts "#{f['term'].ljust(10)} #{f['count']}"
+end
