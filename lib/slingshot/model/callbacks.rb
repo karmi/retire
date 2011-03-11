8b7d7f136e876fb4de6c447edd51abf4a5a6b223
Vojtech Hyza <vhyza@vhyza.eu>
2011-03-11 16:32:38 +0100

    Renamed hook method `<MyModel>.update_index` to `<MyModel>.update_elastic_search_index` (minimize conflicts with existing codebases)

--- lib/slingshot/model/callbacks.rb
+++ lib/slingshot/model/callbacks.rb
@@ -5,8 +5,8 @@ module Slingshot
 
       def self.included(base)
         if base.respond_to?(:after_save) && base.respond_to?(:after_destroy)
-          base.send :after_save,    :update_index
-          base.send :after_destroy, :update_index
+          base.send :after_save,    :update_elastic_search_index
+          base.send :after_destroy, :update_elastic_search_index
         end
       end
 
