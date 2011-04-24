7a1b64fbcdafd7229adaff7f2fa9c21972b8a685
Karel Minarik <karmi@karmi.cz>
2011-04-24 13:42:49 +0200

    [WTF] Fixed test for Index (this should have come from rebase)

--- test/unit/index_test.rb
+++ test/unit/index_test.rb
@@ -109,7 +109,7 @@ module Slingshot
           should "store Hash it under its ID property" do
             Configuration.client.expects(:post).with("#{Configuration.url}/dummy/document/123",
                                                      Yajl::Encoder.encode({:id => 123, :title => 'Test'})).
-                                                returns('{"ok":true,"_id":"123"}')
+                                                returns(mock_response('{"ok":true,"_id":"123"}'))
             @index.store :id => 123, :title => 'Test'
           end
 
