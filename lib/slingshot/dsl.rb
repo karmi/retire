821e838ec7f8e60c7a3043da7969a2d1ff659d50
Karel Minarik <karmi@karmi.cz>
2011-04-26 20:51:59 +0200

    Added the "dual interface" for the `Slingshot` method, allowing it to pass as Ruby Hash or JSON string

--- lib/slingshot/dsl.rb
+++ lib/slingshot/dsl.rb
@@ -6,7 +6,23 @@ module Slingshot
     end
 
     def search(indices, options={}, &block)
-      Search::Search.new(indices, options, &block).perform
+      if block_given?
+        Search::Search.new(indices, options, &block).perform
+      else
+        payload = case options
+          when Hash    then options.to_json
+          when String  then options
+          else raise ArgumentError, "Please pass a Ruby Hash or String with JSON"
+        end
+
+        response = Configuration.client.post( "#{Configuration.url}/#{indices}/_search", payload)
+        json     = Yajl::Parser.parse(response.body)
+        results  = Results::Collection.new(json, options)
+      end
+    rescue Exception => error
+      STDERR.puts "[REQUEST FAILED] #{error.class} #{error.http_body rescue nil}\n"
+      raise
+    ensure
     end
 
     def index(name, &block)
