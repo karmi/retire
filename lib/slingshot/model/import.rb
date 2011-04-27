7277db79a4d9b0aef0db3caafd96877af33033d9
Karel Minarik <karmi@karmi.cz>
2011-04-27 16:01:42 +0200

    [IMPORT] Change the block handling in `import` method: it yields documents, allowing to manipulate them before indexing

--- lib/slingshot/model/import.rb
+++ lib/slingshot/model/import.rb
@@ -10,16 +10,13 @@ module Slingshot
                 "Class '#{self}' must have `paginate` class method (provided eg. by the will_paginate gem) for importing." \
                  unless respond_to?(:paginate)
 
-          options = {:page => 1, :per_page => 1000}.update options
-          total   = self.count rescue nil
-          done    = 0
+          options = {:page => 1, :per_page => 1000}.merge options
 
           while documents = paginate(options.merge :page => options[:page]) and not documents.empty?
+            documents = yield documents if block_given?
+
             index.bulk_store documents
             options[:page] += 1
-            done           += documents.size
-
-            yield total, done if total and block_given?
           end
         end
 
