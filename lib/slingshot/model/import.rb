8034741c4eb16de9c14af1198620cf5d5e0cee72
Karel Minarik <karmi@karmi.cz>
2011-04-28 23:08:51 +0200

    [ACTIVEMODEL] [IMPORT] Refactored `Slingshot::Model::Import.import` implementation to use the new `Index#import` method

--- lib/slingshot/model/import.rb
+++ lib/slingshot/model/import.rb
@@ -5,19 +5,9 @@ module Slingshot
 
       module ClassMethods
 
-        def import options={}
-          raise NoMethodError,
-                "Class '#{self}' must have `paginate` class method (provided eg. by the will_paginate gem) for importing." \
-                 unless respond_to?(:paginate)
-
-          options = {:page => 1, :per_page => 1000}.merge options
-
-          while documents = paginate(options.merge :page => options[:page]) and not documents.empty?
-            documents = yield documents if block_given?
-
-            index.bulk_store documents
-            options[:page] += 1
-          end
+        def import options={}, &block
+          method = options.delete(:method) || 'paginate'
+          self.index.import self, method, options, &block
         end
 
       end
--- test/unit/model_import_test.rb
+++ test/unit/model_import_test.rb
@@ -5,15 +5,19 @@ class ImportModel
   include Slingshot::Model::Search
   include Slingshot::Model::Callbacks
 
-  D = (1..10).to_a
+  DATA = (1..4).to_a
 
   def self.paginate(options={})
-    options = {:page => 0, :per_page => 1000}.update options
-    D.slice( options[:page]*options[:per_page]...(options[:page]+1)*options[:per_page] )
+    options = {:page => 1, :per_page => 1000}.update options
+    DATA.slice( (options[:page]-1)*options[:per_page]...options[:page]*options[:per_page] )
+  end
+
+  def self.all(options={})
+    DATA
   end
 
   def self.count
-    D.size
+    DATA.size
   end
 end
 
@@ -28,24 +32,8 @@ module Slingshot
           assert_respond_to ImportModel, :import
         end
 
-        should "raise errror when model does not have the paginate method" do
-          class ::ModelWithNoPagination
-            extend  ActiveModel::Naming
-            include Slingshot::Model::Search
-            include Slingshot::Model::Callbacks
-          end
-
-          assert_raise(NoMethodError) { ModelWithNoPagination.import }
-        end
-
-        should "paginate the results when importing" do
+        should "paginate the results by default when importing" do
           Slingshot::Index.any_instance.expects(:bulk_store).returns(true).times(2)
-
-          ImportModel.expects(:paginate).
-            returns([1,2]).
-            then.returns([3,4]).
-            then.returns([]).
-            times(3)
 
           ImportModel.import :per_page => 2
         end
@@ -53,12 +41,6 @@ module Slingshot
         should "call the passed block on every batch, and NOT manipulate the documents array" do
           Slingshot::Index.any_instance.expects(:bulk_store).with([1, 2])
           Slingshot::Index.any_instance.expects(:bulk_store).with([3, 4])
-
-          ImportModel.expects(:paginate).
-            returns([1,2]).
-            then.returns([3,4]).
-            then.returns([]).
-            times(3)
 
           runs = 0
           ImportModel.import :per_page => 2 do |documents|
@@ -73,12 +55,6 @@ module Slingshot
         should "manipulate the documents in passed block" do
           Slingshot::Index.any_instance.expects(:bulk_store).with([2, 3])
           Slingshot::Index.any_instance.expects(:bulk_store).with([4, 5])
-
-          ImportModel.expects(:paginate).
-            returns([1,2]).
-            then.returns([3,4]).
-            then.returns([]).
-            times(3)
 
           ImportModel.import :per_page => 2 do |documents|
             # Add 1 to every "document" and return them
