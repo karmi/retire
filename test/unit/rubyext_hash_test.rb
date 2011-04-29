52dc6e3bafe94a35143af535cc35913136a4c4ad
Karel Minarik <karmi@karmi.cz>
2011-04-29 17:33:15 +0200

    [TEST] Fixed an error where test for Hash#to_indexed_json was falsely failing on key order mismatch

--- test/unit/rubyext_hash_test.rb
+++ test/unit/rubyext_hash_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 module Slingshot
 
-  class RubyextHashTest < Test::Unit::TestCase
+  class RubyCoreExtensionsTest < Test::Unit::TestCase
 
     context "Hash" do
 
       should "have to_indexed_json doing the same as to_json" do
         [{}, { 1 => 2 }, { 3 => 4, 5 => 6 }, { nil => [7,8,9] }].each do |h|
-          assert_equal h.to_json, h.to_indexed_json
+          assert_equal Yajl::Parser.parse(h.to_json), Yajl::Parser.parse(h.to_indexed_json)
         end
       end
 
