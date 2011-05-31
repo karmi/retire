7eb130e46209744c56f52c6daa89100de59cf858
Karel Minarik <karmi@karmi.cz>
2011-05-31 17:22:18 +0200

    [RUBY1.9] Disable ActiveRecord integration test on Ruby 1.9, since the SQLite adapter gives segmentation faults

--- test/integration/active_record_searchable_test.rb
+++ test/integration/active_record_searchable_test.rb
@@ -1,185 +1,191 @@
 require 'test_helper'
 
-module Tire
+# TODO: Figure out why activerecord-3.0.7/lib/active_record/connection_adapters/sqlite3_adapter.rb
+#       gives Segmentation fault. For now, the test is simply disabled.
+#
+if RUBY_VERSION < "1.9"
+  module Tire
 
-  class ActiveRecordSearchableIntegrationTest < Test::Unit::TestCase
-    include Test::Integration
+    class ActiveRecordSearchableIntegrationTest < Test::Unit::TestCase
+      include Test::Integration
 
-    def setup
-      super
-      File.delete fixtures_path.join('articles.db') rescue nil
+      def setup
+        super
+        File.delete fixtures_path.join('articles.db') rescue nil
 
-      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database => fixtures_path.join('articles.db') )
+        ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database => fixtures_path.join('articles.db') )
 
-      ActiveRecord::Migration.verbose = false
-      ActiveRecord::Schema.define(:version => 1) do
-        create_table :active_record_articles do |t|
-          t.string   :title
-          t.datetime :created_at, :default => 'NOW()'
+        ActiveRecord::Migration.verbose = false
+        ActiveRecord::Schema.define(:version => 1) do
+          create_table :active_record_articles do |t|
+            t.string   :title
+            t.datetime :created_at, :default => 'NOW()'
+          end
         end
       end
-    end
 
-    def teardown
-      super
-      File.delete fixtures_path.join('articles.db') rescue nil
-    end
-
-    context "ActiveRecord integration" do
-
-      setup    do
-        Tire.index('active_record_articles').delete
-        load File.expand_path('../../models/active_record_article.rb', __FILE__)
-      end
-      teardown { Tire.index('active_record_articles').delete }
-
-      should "configure mapping" do
-        assert_equal 'snowball', ActiveRecordArticle.mapping[:title][:analyzer]
-        assert_equal 10, ActiveRecordArticle.mapping[:title][:boost]
-
-        assert_equal 'snowball', ActiveRecordArticle.elasticsearch_index.mapping['active_record_article']['properties']['title']['analyzer']
+      def teardown
+        super
+        File.delete fixtures_path.join('articles.db') rescue nil
       end
 
-      should "save document into index on save and find it" do
-        a = ActiveRecordArticle.new :title => 'Test'
-        a.save!
-        id = a.id
+      context "ActiveRecord integration" do
 
-        a.index.refresh
-        sleep(1.5) # Leave ES some breathing room here...
+        setup    do
+          Tire.index('active_record_articles').delete
+          load File.expand_path('../../models/active_record_article.rb', __FILE__)
+        end
+        teardown { Tire.index('active_record_articles').delete }
 
-        results = ActiveRecordArticle.search 'test'
+        should "configure mapping" do
+          assert_equal 'snowball', ActiveRecordArticle.mapping[:title][:analyzer]
+          assert_equal 10, ActiveRecordArticle.mapping[:title][:boost]
 
-        assert_equal 1, results.count
+          assert_equal 'snowball', ActiveRecordArticle.elasticsearch_index.mapping['active_record_article']['properties']['title']['analyzer']
+        end
 
-        assert_instance_of ActiveRecordArticle, results.first
-        assert_not_nil results.first.id
-        assert_equal   id, results.first.id
-        assert         results.first.persisted?, "Record should be persisted"
-        assert_not_nil results.first._score
-        assert_equal   'Test', results.first.title
-      end
+        should "save document into index on save and find it" do
+          a = ActiveRecordArticle.new :title => 'Test'
+          a.save!
+          id = a.id
 
-      should "remove document from index on destroy" do
-        a = ActiveRecordArticle.new :title => 'Test'
-        a.save!
-        a.destroy
+          a.index.refresh
+          sleep(1.5) # Leave ES some breathing room here...
 
-        a.index.refresh
-        results = ActiveRecordArticle.search 'test'
+          results = ActiveRecordArticle.search 'test'
+
+          assert_equal 1, results.count
+
+          assert_instance_of ActiveRecordArticle, results.first
+          assert_not_nil results.first.id
+          assert_equal   id, results.first.id
+          assert         results.first.persisted?, "Record should be persisted"
+          assert_not_nil results.first._score
+          assert_equal   'Test', results.first.title
+        end
+
+        should "remove document from index on destroy" do
+          a = ActiveRecordArticle.new :title => 'Test'
+          a.save!
+          a.destroy
+
+          a.index.refresh
+          results = ActiveRecordArticle.search 'test'
         
-        assert_equal 0, results.count
-      end
+          assert_equal 0, results.count
+        end
 
-      should "return documents with scores" do
-        ActiveRecordArticle.create! :title => 'foo'
-        ActiveRecordArticle.create! :title => 'bar'
+        should "return documents with scores" do
+          ActiveRecordArticle.create! :title => 'foo'
+          ActiveRecordArticle.create! :title => 'bar'
 
-        ActiveRecordArticle.elasticsearch_index.refresh
-        results = ActiveRecordArticle.search 'foo OR bar^100'
-        assert_equal 2, results.count
-
-        assert_equal 'bar', results.first.title
-      end
-
-      context "with pagination" do
-        setup do
-          1.upto(9) { |number| ActiveRecordArticle.create :title => "Test#{number}" }
           ActiveRecordArticle.elasticsearch_index.refresh
+          results = ActiveRecordArticle.search 'foo OR bar^100'
+          assert_equal 2, results.count
+
+          assert_equal 'bar', results.first.title
         end
 
-        context "and parameter searches" do
-
-          should "find first page with five results" do
-            results = ActiveRecordArticle.search 'test*', :sort => 'title', :per_page => 5, :page => 1
-            assert_equal 5, results.size
-
-            assert_equal 2, results.total_pages
-            assert_equal 1, results.current_page
-            assert_equal nil, results.previous_page
-            assert_equal 2, results.next_page
-
-            assert_equal 'Test1', results.first.title
+        context "with pagination" do
+          setup do
+            1.upto(9) { |number| ActiveRecordArticle.create :title => "Test#{number}" }
+            ActiveRecordArticle.elasticsearch_index.refresh
           end
 
-          should "find next page with five results" do
-            results = ActiveRecordArticle.search 'test*', :sort => 'title', :per_page => 5, :page => 2
-            assert_equal 4, results.size
+          context "and parameter searches" do
 
-            assert_equal 2, results.total_pages
-            assert_equal 2, results.current_page
-            assert_equal 1, results.previous_page
-            assert_equal nil, results.next_page
+            should "find first page with five results" do
+              results = ActiveRecordArticle.search 'test*', :sort => 'title', :per_page => 5, :page => 1
+              assert_equal 5, results.size
 
-            assert_equal 'Test6', results.first.title
-          end
+              assert_equal 2, results.total_pages
+              assert_equal 1, results.current_page
+              assert_equal nil, results.previous_page
+              assert_equal 2, results.next_page
 
-          should "find not find missing page" do
-            results = ActiveRecordArticle.search 'test*', :sort => 'title', :per_page => 5, :page => 3
-            assert_equal 0, results.size
-
-            assert_equal 2, results.total_pages
-            assert_equal 3, results.current_page
-            assert_equal 2, results.previous_page
-            assert_equal nil, results.next_page
-
-            assert_nil results.first
-          end
-
-        end
-
-        context "and block searches" do
-          setup { @q = 'test*' }
-
-          should "find first page with five results" do
-            results = ActiveRecordArticle.search do |search|
-              search.query { |query| query.string @q }
-              search.sort  { title }
-              search.from 0
-              search.size 5
+              assert_equal 'Test1', results.first.title
             end
-            assert_equal 5, results.size
 
-            assert_equal 2, results.total_pages
-            assert_equal 1, results.current_page
-            assert_equal nil, results.previous_page
-            assert_equal 2, results.next_page
+            should "find next page with five results" do
+              results = ActiveRecordArticle.search 'test*', :sort => 'title', :per_page => 5, :page => 2
+              assert_equal 4, results.size
 
-            assert_equal 'Test1', results.first.title
+              assert_equal 2, results.total_pages
+              assert_equal 2, results.current_page
+              assert_equal 1, results.previous_page
+              assert_equal nil, results.next_page
+
+              assert_equal 'Test6', results.first.title
+            end
+
+            should "find not find missing page" do
+              results = ActiveRecordArticle.search 'test*', :sort => 'title', :per_page => 5, :page => 3
+              assert_equal 0, results.size
+
+              assert_equal 2, results.total_pages
+              assert_equal 3, results.current_page
+              assert_equal 2, results.previous_page
+              assert_equal nil, results.next_page
+
+              assert_nil results.first
+            end
+
           end
 
-          should "find next page with five results" do
-            results = ActiveRecordArticle.search do |search|
-              search.query { |query| query.string @q }
-              search.sort  { title }
-              search.from 5
-              search.size 5
+          context "and block searches" do
+            setup { @q = 'test*' }
+
+            should "find first page with five results" do
+              results = ActiveRecordArticle.search do |search|
+                search.query { |query| query.string @q }
+                search.sort  { title }
+                search.from 0
+                search.size 5
+              end
+              assert_equal 5, results.size
+
+              assert_equal 2, results.total_pages
+              assert_equal 1, results.current_page
+              assert_equal nil, results.previous_page
+              assert_equal 2, results.next_page
+
+              assert_equal 'Test1', results.first.title
             end
-            assert_equal 4, results.size
 
-            assert_equal 2, results.total_pages
-            assert_equal 2, results.current_page
-            assert_equal 1, results.previous_page
-            assert_equal nil, results.next_page
+            should "find next page with five results" do
+              results = ActiveRecordArticle.search do |search|
+                search.query { |query| query.string @q }
+                search.sort  { title }
+                search.from 5
+                search.size 5
+              end
+              assert_equal 4, results.size
 
-            assert_equal 'Test6', results.first.title
-          end
+              assert_equal 2, results.total_pages
+              assert_equal 2, results.current_page
+              assert_equal 1, results.previous_page
+              assert_equal nil, results.next_page
 
-          should "not find a missing page" do
-            results = ActiveRecordArticle.search do |search|
-              search.query { |query| query.string @q }
-              search.sort  { title }
-              search.from 10
-              search.size 5
+              assert_equal 'Test6', results.first.title
             end
-            assert_equal 0, results.size
 
-            assert_equal 2, results.total_pages
-            assert_equal 3, results.current_page
-            assert_equal 2, results.previous_page
-            assert_equal nil, results.next_page
+            should "not find a missing page" do
+              results = ActiveRecordArticle.search do |search|
+                search.query { |query| query.string @q }
+                search.sort  { title }
+                search.from 10
+                search.size 5
+              end
+              assert_equal 0, results.size
 
-            assert_nil results.first
+              assert_equal 2, results.total_pages
+              assert_equal 3, results.current_page
+              assert_equal 2, results.previous_page
+              assert_equal nil, results.next_page
+
+              assert_nil results.first
+            end
+
           end
 
         end
@@ -189,5 +195,4 @@ module Tire
     end
 
   end
-
 end
