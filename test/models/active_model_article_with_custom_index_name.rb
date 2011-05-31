1f956d3e453c8140f6c15c935e33ae83d545f2c0
Karel Minarik <karmi@karmi.cz>
2011-05-31 18:12:49 +0200

    [FIX] Load 'active_model_article' in 'active_model_article_with_custom_index_name' test model class

--- test/models/active_model_article_with_custom_index_name.rb
+++ test/models/active_model_article_with_custom_index_name.rb
@@ -1,5 +1,7 @@
 # Example ActiveModel class with custom index name
 
+require File.expand_path('../active_model_article', __FILE__)
+
 class ActiveModelArticleWithCustomIndexName < ActiveModelArticle
   index_name 'custom-index-name'
 end
