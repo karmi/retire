577b419575354b0d78e7462e9e0ab997950b999e
Karel Minarik <karmi@karmi.cz>
2011-06-27 15:13:00 +0200

    Release 0.1.14

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,15 +1,12 @@
 module Tire
-  VERSION          = "0.1.13"
+  VERSION          = "0.1.14"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
     # Added `<after/before>_update_elastic_search_index` callbacks for models
-    # Search performs GET instead of POST
-    # Added percolator support
-    # Added percolator support for models
-    # CHANGELOG support in gemspec
-    # [FIX] Do not redefine #to_hash in models
-    # [FIX] Added that MyModel#update_elastic_search_index sets _index, _type, _version properties
+    # Added percolator support for DSL and ActiveModel
+    # Lazy loading of ResultsCollection into memory
+    # Fixes for debug logging
   END
 end
