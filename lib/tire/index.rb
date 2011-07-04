df048a8afd38d3a70812271226245d751e6d8039
Karel Minarik <karmi@karmi.cz>
2011-07-04 19:05:42 +0200

    Changed, that Index#import checks for `documents.size > 0`, not `documents.empty?`, so Mongoid/Kaminari can do their funky stuff

--- lib/tire/index.rb
+++ lib/tire/index.rb
@@ -134,7 +134,7 @@ module Tire
         when method
           options = {:page => 1, :per_page => 1000}.merge options
           while documents = klass_or_collection.send(method.to_sym, options.merge(:page => options[:page])) \
-                            and not documents.empty?
+                            and documents.size > 0
             documents = yield documents if block_given?
 
             bulk_store documents
