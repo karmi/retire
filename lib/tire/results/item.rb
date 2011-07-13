3487b2582c4284652ae006dfc32f2765263a8628
Karel Minarik <karmi@karmi.cz>
2011-07-13 11:21:55 +0200

    Changed that Results::Item does uses delegation to Hash, not inheritance from Hash
    (Anything which `is_a?(Hash)` breaks routes and other stuff in Rails.)

--- lib/tire/results/item.rb
+++ lib/tire/results/item.rb
@@ -1,15 +1,16 @@
 module Tire
   module Results
 
-    class Item < Hash
+    class Item
 
       # Create new instance, recursively converting all Hashes to Item
       # and leaving everything else alone.
       #
       def initialize(args={})
         raise ArgumentError, "Please pass a Hash-like object" unless args.respond_to?(:each_pair)
+        @attributes = {}
         args.each_pair do |key, value|
-          self[key.to_sym] = value.is_a?(Hash) ? self.class.new(value.to_hash) : value
+          @attributes[key.to_sym] = value.is_a?(Hash) ? self.class.new(value.to_hash) : value
         end
       end
 
@@ -17,13 +18,15 @@ module Tire
       # otherwise return +nil+.
       #
       def method_missing(method_name, *arguments)
-        self.has_key?(method_name.to_sym) ? self[method_name.to_sym] : nil
+        @attributes.has_key?(method_name.to_sym) ? @attributes[method_name.to_sym] : nil
       end
 
-      # Get ID
-      #
+      def [](key)
+        @attributes[key]
+      end
+
       def id
-        self[:id]
+        @attributes[:_id] || @attributes[:id]
       end
 
       def persisted?
@@ -31,10 +34,13 @@ module Tire
       end
 
       def inspect
-        s = []; self.each { |k,v| s << "#{k}: #{v.inspect}" }
+        s = []; @attributes.each { |k,v| s << "#{k}: #{v.inspect}" }
         %Q|<Item #{s.join(', ')}>|
       end
 
+      def to_json(options={})
+        @attributes.to_json(options)
+      end
       alias_method :to_indexed_json, :to_json
 
     end
