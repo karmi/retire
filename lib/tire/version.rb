7044f6bf039065c578f65c1a8eb82065745769d0
Karel Minarik <karmi@karmi.cz>
2011-07-14 19:43:02 +0200

    Release 0.1.16

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,12 +1,13 @@
 module Tire
-  VERSION          = "0.1.15"
+  VERSION   = "0.1.16"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    # Cleanup of code for getting document type, id, JSON serialization
-    # Bunch of deprecations: sorting, passing document type to store/remove
-    # Displaying a warning when no ID is passed when storing in bulk
-    # Correct handling of import for Mongoid/Kaminari combos
+    # Defined mapping for nested fields [#56]
+    # Mapping type is optional and defaults to "string"
+    # Fixed handling of fields returned prefixed by _source from ES [#31]
+    # Allow passing the type to search and added that model passes `document_type` to search [@jonkarna, #38]
+    # Allow leaving index name empty for searching the whole server
   END
 end
