152197bb8ce023edc2b0dcb8308a7c8e880d81c6
Andrew Timberlake <andrew@andrewtimberlake.com>
2011-07-29 16:15:28 +0200

    Moving size and from calls to outside the block check so that options can be used for pagination

--- lib/tire/model/search.rb
+++ lib/tire/model/search.rb
@@ -39,8 +39,10 @@ module Tire
           sort    = Array( options[:order] || options[:sort] )
           options = {:type => document_type}.update(options)
 
+          s = Tire::Search::Search.new(elasticsearch_index.name, options)
+          s.size( options[:per_page].to_i ) if options[:per_page]
+          s.from( options[:page].to_i <= 1 ? 0 : (options[:per_page].to_i * (options[:page].to_i-1)) ) if options[:page] && options[:per_page]
           unless block_given?
-            s = Tire::Search::Search.new(elasticsearch_index.name, options)
             s.query { string query }
             s.sort do
               sort.each do |t|
@@ -48,14 +50,10 @@ module Tire
                 by field_name, direction
               end
             end unless sort.empty?
-            s.size( options[:per_page].to_i ) if options[:per_page]
-            s.from( options[:page].to_i <= 1 ? 0 : (options[:per_page].to_i * (options[:page].to_i-1)) ) if options[:page] && options[:per_page]
-            s.perform.results
           else
-            s = Tire::Search::Search.new(elasticsearch_index.name, options)
             block.arity < 1 ? s.instance_eval(&block) : block.call(s)
-            s.perform.results
           end
+          s.perform.results
         ensure
           Tire::Configuration.wrapper old_wrapper
         end
