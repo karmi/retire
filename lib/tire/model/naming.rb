ea3da8c3b0bbe14db144f3de59ca86ec86f64933
Karel Minarik <karmi@karmi.cz>
2011-09-04 09:38:49 +0200

    [DOC] Added documentation for the Model::Naming module

--- lib/tire/model/naming.rb
+++ lib/tire/model/naming.rb
@@ -1,24 +1,44 @@
 module Tire
   module Model
 
+    # Contains logic for getting and setting the index name and document type for this model.
+    #
     module Naming
 
       module ClassMethods
+
+        # Get or set the index name for this model, based on arguments.
+        #
+        # To get the index name:
+        #
+        #     Article.index_name
+        #
+        # To set the index name:
+        #
+        #     Article.index_name 'my-custom-name'
+        #
         def index_name name=nil
           @index_name = name if name
           @index_name || klass.model_name.plural
         end
 
+        # Get the document type for this model, based on the class name.
+        #
         def document_type
           klass.model_name.singular
         end
       end
 
       module InstanceMethods
+
+        # Proxy to class method `index_name`.
+        #
         def index_name
           instance.class.tire.index_name
         end
 
+        # Proxy to instance method `document_type`.
+        #
         def document_type
           instance.class.tire.document_type
         end
