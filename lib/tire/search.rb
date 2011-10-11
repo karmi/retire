a13eaaaa149db5ea5649083b297286a8310916eb
rubish <rubish@rubish.(none)>
2011-10-12 03:02:57 +0530

    add support to join mutiple filters with 'and'

--- lib/tire/search.rb
+++ lib/tire/search.rb
@@ -89,7 +89,8 @@ module Tire
         request.update( { :query  => @query.to_hash } )    if @query
         request.update( { :sort   => @sort.to_ary   } )    if @sort
         request.update( { :facets => @facets.to_hash } )   if @facets
-        @filters.each { |filter| request.update( { :filter => filter.to_hash } ) } if @filters
+        request.update( { :filter => @filters.first.to_hash } ) if @filters && @filters.size == 1
+        request.update( { :filter => { :and => @filters.inject([]) { |filters, filter| filters << filter.to_hash } } } ) if  @filters && @filters.size > 1
         request.update( { :highlight => @highlight.to_hash } ) if @highlight
         request.update( { :size => @size } )               if @size
         request.update( { :from => @from } )               if @from
