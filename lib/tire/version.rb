cb4228954bd12618bee4dfe2c533a7a49a68219b
Karel Minarik <karmi@karmi.cz>
2011-10-18 14:20:07 +0200

    Release 0.3.6

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,10 +1,12 @@
 module Tire
-  VERSION   = "0.3.5"
+  VERSION   = "0.3.6"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    # Added a "query" facet for free-form aggregations
+    * Fixed a bug: `Results::Item` was referencing Ruby classes incorrectly within Rails
+    * Added support for the Analyze API (#124)
+    * Added support for adding multiple filters (#122)
 
   END
 end
