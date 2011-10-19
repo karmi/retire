a257260c83165f09160018033f4f6ccd6aa22799
Karel Minarik <karmi@karmi.cz>
2011-10-19 10:35:07 +0200

    Release 0.3.7

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,5 +1,5 @@
 module Tire
-  VERSION   = "0.3.6"
+  VERSION   = "0.3.7"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
@@ -7,6 +7,7 @@ module Tire
     * Fixed a bug: `Results::Item` was referencing Ruby classes incorrectly within Rails
     * Added support for the Analyze API (#124)
     * Added support for adding multiple filters (#122)
-
+    * Fixed incorrect passing of options in the `date` and `terms` facets
+    * Removed SDoc since it breaks with current RDoc
   END
 end
