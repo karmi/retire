5e8da81802af0cc2d995dfa4e2cff882a8f985f1
Karel Minarik <karmi@karmi.cz>
2011-11-11 17:58:51 +0100

    Release 0.3.11

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,10 +1,11 @@
 module Tire
-  VERSION   = "0.3.10"
+  VERSION   = "0.3.11"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
     * Fixes and improvements for Logger    [#142]
     * Better handling of search exceptions [#145]
+    * Removed pinning Rake to specific version
   END
 end
