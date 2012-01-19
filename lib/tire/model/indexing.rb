039cc091309a11363593281f5ff405b4ca974b65
Woody Peterson <woody.peterson@gmail.com>
2012-01-18 18:08:55 -0800

    [ACTIVEMODEL] Support any level of mappings in `mapping`
    Previously, only two levels of mappings were supported, ex:
    
        mapping do
          indexes :title, :type => 'string'
          indexes :author do
            indexes :first_name, :type => 'string'
            indexes :last_name,  :type => 'string', :boost => 100
          end
        end
    
    This commit adds support for any level of mappings, ex:
    
        mapping do
          indexes :title, :type => 'string'
          indexes :author do
            indexes :first_name, :type => 'string'
            indexes :last_name,  :type => 'string', :boost => 100
    
            indexes :posts do
              indexes :title, :type => 'string', :boost => 10
            end
          end
        end
    
    Closes #210.

--- lib/tire/model/indexing.rb
+++ lib/tire/model/indexing.rb
@@ -41,6 +41,14 @@ module Tire
         #         indexes :id,    :index    => :not_analyzed
         #         indexes :title, :analyzer => 'snowball', :boost => 100
         #         indexes :words, :as       => 'content.split(/\W/).length'
+        #
+        #         indexes :comments do
+        #           indexes :body
+        #           indexes :author do
+        #             indexes :name
+        #           end
+        #         end
+        #
         #         # ...
         #       end
         #     end
@@ -76,21 +84,21 @@ module Tire
         # for more information.
         #
         def indexes(name, options = {}, &block)
+          mapping[name] = options
+
           if block_given?
-            mapping[name] ||= { :type => 'object', :properties => {} }.update(options)
-            @_nested_mapping = name
-            nested = yield
-            @_nested_mapping = nil
-            self
-          else
-            options[:type] ||= 'string'
-            if @_nested_mapping
-              mapping[@_nested_mapping][:properties][name] = options
-            else
-              mapping[name] = options
-            end
-            self
+            mapping[name][:type]       ||= 'object'
+            mapping[name][:properties] ||= {}
+
+            previous = @mapping
+            @mapping = mapping[name][:properties]
+            yield
+            @mapping = previous
           end
+
+          mapping[name][:type] ||= 'string'
+
+          self
         end
 
         # Creates the corresponding index with desired settings and mappings, when it does not exists yet.
