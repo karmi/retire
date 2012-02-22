9ff44304b7e5660b73f4d68f89e33b3cf4c655f1
Nitish Sharma <nitish@skylin.es>
2012-02-22 10:52:29 +0100

    unit test for parent/child feature

--- test/unit/bulk_parent_test.rb
+++ test/unit/bulk_parent_test.rb
@@ -33,7 +33,14 @@ module Tire
         @index.bulk_store [ {:id => '1', :title => 'One', :_parent => "abcdef", :type => "blog_tag"}, {:id => '2', :title => 'Two', :type => "blog_tag", :_parent => "qwerty"} ]
       end
 
-      should "bulk store should handle the 'parent' field"
+      should "bulk store should handle the 'parent' field" do
+        Configuration.client.expects(:post).with do |url, json|
+          url  == "#{Configuration.url}/_bulk" &&
+            json =~ /\A{.*?"_parent":"abcdef".*?}}/
+        end.returns(mock_response('{}'), 200)
+
+          @index.bulk_store [ {:id => '1', :title => 'One', :parent => "abcdef", :type => "blog_tag"}, {:id => '2', :title => 'Two', :type => "blog_tag", :parent => "qwerty"} ]
+      end
     end
   end
 end
