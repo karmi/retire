4a92865fec4524561abfe1aa7b4ff1d9cc59ac5a
Aldo Sarmiento <sarmiena@gmail.com>
2012-03-25 04:14:31 -0700

    [#218] Fixed the incorrect serialization of `document_type` in Index#bulk_store
    Previously, when storing individual namespaced records, it correctly stored them as "my_namespace/my_model",
    but bulk_store was saving _type as "my_namespace%2Fmy_model".
    
    This resulted in two different document types where there should only be one, incorrect mapping, incorrect searches.
    
    Closes #290, closes #291, fixes #218.

--- lib/tire/index.rb
+++ lib/tire/index.rb
@@ -66,8 +66,8 @@ module Tire
 
     def bulk_store(documents, options={})
       payload = documents.map do |document|
+        type = get_type_from_document(document, :escape => false) # Do not URL-escape the _type
         id   = get_id_from_document(document)
-        type = get_type_from_document(document)
 
         STDERR.puts "[ERROR] Document #{document.inspect} does not have ID" unless id
 
@@ -265,7 +265,9 @@ module Tire
       end
     end
 
-    def get_type_from_document(document)
+    def get_type_from_document(document, options={})
+      options = {:escape => true}.merge(options)
+
       old_verbose, $VERBOSE = $VERBOSE, nil # Silence Object#type deprecation warnings
       type = case
         when document.respond_to?(:document_type)
@@ -278,7 +280,9 @@ module Tire
           document.type
         end
       $VERBOSE = old_verbose
-      Utils.escape( type || 'document' )
+
+      type ||= 'document'
+      options[:escape] ? Utils.escape(type) : type
     end
 
     def get_id_from_document(document)
