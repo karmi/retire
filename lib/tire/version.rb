8ab5774efa4ef8c9ff23b5be0c35c3821931367e
Karel Minarik <karmi@karmi.cz>
2012-03-28 12:10:09 +0200

    Release 0.4.0

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,12 +1,12 @@
 module Tire
-  VERSION   = "0.4.0.rc"
+  VERSION   = "0.4.0"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    * Added support for property defaults and casting model properties as Ruby objects in Tire::Model::Persistence
+    * Persistence supports property defaults and casting model properties as Ruby objects
     * Added Hashr (http://rubygems.org/gems/hashr) as dependency
-    * Changed that search in persistence returns instances of model not Item
+    * Search in persistence models returns model instances, not Items
     * Fixed errors in the Curb client
     * Re-raise the RestClient::RequestTimeout and RestClient::ServerBrokeConnection exceptions
     * Index#bulk_store and Index#import support the `:raise` option to re-raise exceptions
@@ -15,13 +15,13 @@ module Tire
     * Deprecated the support for passing JSON strings to `Index#store`
     * ActiveModel mapping has the `:as` option dynamically set property value for serialization
     * ActiveModel supports any level of mappings in `mapping`
-    * ActiveModel integration now can eagerly load records of multiple types/classes
+    * ActiveModel search can eagerly load records of multiple types/classes
     * ActiveModel integration now properly supports namespaced models
     * Added support for passing search params (`search_type`, `timeout`, etc.) to search requests
     * Added the "tire:index:drop" Rake task
-    * Added the "Filter Facets" support
+    * Added the "Filter" facet type
     * Added the "Fuzzy" search query type
-    * Test suite refactorings and changes
+    * Various test suite refactorings and changes
     * Relaxed gem dependencies
   END
 end
