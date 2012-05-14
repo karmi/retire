26eb6163fdbbaaa6568dbfdc20cbfc5de9fa04d3
Roman Shterenzon <roman.shterenzon@gmail.com>
2012-05-15 01:30:54 +0300

    Configuration example

--- lib/tire/http/clients/faraday.rb
+++ lib/tire/http/clients/faraday.rb
@@ -1,5 +1,18 @@
 require 'faraday'
 
+# Example:
+#
+# require 'tire/http/clients/faraday'
+# Tire.configure do |config|
+#   # Unless specified, tire will use Faraday.default_adapter and no middleware
+#   Tire::HTTP::Client::Faraday.faraday_middleware = Proc.new do |builder|
+#     builder.adapter :net_http_persistent
+#     builder.use FaradayMiddleware::Instrumentation, :name => 'request.tire'
+#   end
+#
+#   config.client(Tire::HTTP::Client::Faraday)
+# end
+
 module Tire
   module HTTP
     module Client
