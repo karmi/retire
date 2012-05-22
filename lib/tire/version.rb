f160f4c165007c958c26aee061d33786bddd8f66
Tim Pease <tim.pease@gmail.com>
2012-05-22 09:19:29 -0600

    version bump

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,8 +1,19 @@
 module Tire
-  VERSION   = "0.4.2"
+  VERSION = "0.4.2.1".freeze
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
+
+    Version 0.4.2.1
+    ---------------
+    * Removed all depedence on ActiveModel
+    * Fixed tests to work with any version of ActiveSupport
+
+    Version 0.4.2
+    -------------
+    * Fixed incorrect handling of PUT requests in the Curb client
+    * Fixed, that blocks passed to `Tire::Index.new` or `Tire.index` losed the scope
+    * Added `Tire::Alias`, interface and DSL to manage aliases as resources
 
     Version 0.4.1
     -------------
@@ -12,11 +23,5 @@ module Tire
     * Added basic support for index aliases
     * Changed, that Index#bulk_store runs against an index endpoint, not against `/_bulk`
     * Refactorings, fixes, Ruby 1.8 compatibility
-
-    Version 0.4.2
-    -------------
-    * Fixed incorrect handling of PUT requests in the Curb client
-    * Fixed, that blocks passed to `Tire::Index.new` or `Tire.index` losed the scope
-    * Added `Tire::Alias`, interface and DSL to manage aliases as resources
   END
 end
