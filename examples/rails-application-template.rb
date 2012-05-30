dab3147e79edabd9200e217a6f3bc0dc38ba7809
Marek Stachura <marek@dedico.pl>
2012-05-30 15:38:25 +0200

    Added `attr_accessible` to Article model in the template for example Rails application
    See <http://api.rubyonrails.org/classes/ActiveModel/MassAssignmentSecurity/ClassMethods.html#method-i-attr_accessible>
    
    Closes #355

--- examples/rails-application-template.rb
+++ examples/rails-application-template.rb
@@ -169,6 +169,8 @@ file 'app/models/article.rb', <<-CODE
 class Article < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
+
+  attr_accessible :title, :content, :published_on
 end
 CODE
 
