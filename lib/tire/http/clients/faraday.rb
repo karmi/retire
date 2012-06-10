ee607048917f0d150274d4c3a0b954e250669e61
Karel Minarik <karmi@karmi.cz>
2012-06-10 10:54:17 +0200

    Fix incorrect/incomplete Faraday usage instructions
    Closes #337.

--- lib/tire/http/clients/faraday.rb
+++ lib/tire/http/clients/faraday.rb
@@ -9,14 +9,14 @@ require 'faraday'
 # Example:
 # --------
 #
+#     require 'typhoeus'
 #     require 'tire/http/clients/faraday'
 #
 #     Tire.configure do |config|
 #     
 #       # Unless specified, tire will use Faraday.default_adapter and no middleware
 #       Tire::HTTP::Client::Faraday.faraday_middleware = Proc.new do |builder|
-#         builder.adapter :net_http_persistent
-#         builder.use FaradayMiddleware::Instrumentation, :name => 'request.tire'
+#         builder.adapter :typhoeus
 #       end
 #     
 #       config.client(Tire::HTTP::Client::Faraday)
