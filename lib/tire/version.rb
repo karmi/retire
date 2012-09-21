4132c1eeb5dc96f897cf06e1a60e2045efbc46dd
Karel Minarik <karmi@karmi.cz>
2012-09-21 18:13:22 +0200

    Release 0.4.3

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,22 +1,15 @@
 module Tire
-  VERSION   = "0.4.2"
+  VERSION   = "0.4.3"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    Version 0.4.1
-    -------------
-    * Added a Index#settings method to retrieve index settings as a Hash
-    * Added support for the "scan" search in the Ruby API
-    * Added support for reindexing the index documents into new index
-    * Added basic support for index aliases
-    * Changed, that Index#bulk_store runs against an index endpoint, not against `/_bulk`
-    * Refactorings, fixes, Ruby 1.8 compatibility
-
-    Version 0.4.2
-    -------------
-    * Fixed incorrect handling of PUT requests in the Curb client
-    * Fixed, that blocks passed to `Tire::Index.new` or `Tire.index` losed the scope
-    * Added `Tire::Alias`, interface and DSL to manage aliases as resources
+    * Added a prefix query
+    * Added support for "script fields" (return a script evaluation for each hit)
+    * Added support for the Update API in Tire::Index
+    * [FIX] Fixed incorrect `Results::Item#to_hash` serialization
+    * 730813f Added support for aggregating over multiple fields in the "terms" facet
+    * Added the "Dis Max Query"
+    * Added the ability to transform documents when reindexing
   END
 end
