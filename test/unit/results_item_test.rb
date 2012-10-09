35e8a5f28c9dbd8c0372a3ced567f84948edb905
Sławosz <slawosz@gmail.com>
2012-10-09 14:08:11 +0200

    [FIX] Implemented `respond_to?` for `Item`
    Closes #473

--- test/unit/results_item_test.rb
+++ test/unit/results_item_test.rb
@@ -65,6 +65,10 @@ module Tire
         assert_equal 'Test', @document.title
       end
 
+      should "implement respond_to? for proxied methods" do
+        assert @document.respond_to?(:title)
+      end
+
       should "return nil for non-existing keys/methods" do
         assert_nothing_raised { @document.whatever }
         assert_nil @document.whatever
