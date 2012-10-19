bac091aad6581ef5d7064ff7f0140cbd874f4694
Karel Minarik <karmi@karmi.cz>
2012-10-19 23:07:48 +0200

    [REFACTORING] Extracted logic in `Tire::Results::Collection#results` into separate methods
    Refactored the ugly ~ 50LOC mess in `Tire::Results::Collection#results` into separate methods:
    
    * __get_results_without_load
    * __get_results_with_load
    * __find_records_by_ids
    
    This not only makes the codebase cleaner, but also provides hook points for people
    who need to customize eg. the loading behaviour for Activerecord, where something like:
    
        def __find_records_by_ids(klass, ids)
          @options[:load] === true ? klass.find_all_by_id(ids) : klass.find_all_by_id(ids, @options[:load])
        end
    
    prevents `RecordNotFound` exceptions on missing records (and other scenarios).
    
    Fixes #343, fixes #485.

--- lib/tire/results/collection.rb
+++ lib/tire/results/collection.rb
@@ -20,48 +20,10 @@ module Tire
       def results
         @results ||= begin
           hits = @response['hits']['hits'].map { |d| d.update '_type' => Utils.unescape(d['_type']) }
-
           unless @options[:load]
-            if @wrapper == Hash
-              hits
-            else
-              hits.map do |h|
-                document = {}
-
-                # Update the document with content and ID
-                document = h['_source'] ? document.update( h['_source'] || {} ) : document.update( __parse_fields__(h['fields']) )
-                document.update( {'id' => h['_id']} )
-
-                # Update the document with meta information
-                ['_score', '_type', '_index', '_version', 'sort', 'highlight', '_explanation'].each { |key| document.update( {key => h[key]} || {} ) }
-
-                # Return an instance of the "wrapper" class
-                @wrapper.new(document)
-              end
-            end
-
+            __get_results_without_load(hits)
           else
-            return [] if hits.empty?
-
-            records = {}
-            @response['hits']['hits'].group_by { |item| item['_type'] }.each do |type, items|
-              raise NoMethodError, "You have tried to eager load the model instances, " +
-                                   "but Tire cannot find the model class because " +
-                                   "document has no _type property." unless type
-
-              begin
-                klass = type.camelize.constantize
-              rescue NameError => e
-                raise NameError, "You have tried to eager load the model instances, but " +
-                                 "Tire cannot find the model class '#{type.camelize}' " +
-                                 "based on _type '#{type}'.", e.backtrace
-              end
-              ids = items.map { |h| h['_id'] }
-              records[type] = @options[:load] === true ? klass.find(ids) : klass.find(ids, @options[:load])
-            end
-
-            # Reorder records to preserve order from search results
-            @response['hits']['hits'].map { |item| records[item['_type']].detect { |record| record.id.to_s == item['_id'].to_s } }
+            __get_results_with_load(hits)
           end
         end
       end
@@ -108,6 +70,58 @@ module Tire
           fields.update result
         end
         fields
+      end
+
+      def __get_results_without_load(hits)
+        if @wrapper == Hash
+          hits
+        else
+          hits.map do |h|
+            document = {}
+
+            # Update the document with content and ID
+            document = h['_source'] ? document.update( h['_source'] || {} ) : document.update( __parse_fields__(h['fields']) )
+            document.update( {'id' => h['_id']} )
+
+            # Update the document with meta information
+            ['_score', '_type', '_index', '_version', 'sort', 'highlight', '_explanation'].each { |key| document.update( {key => h[key]} || {} ) }
+
+            # Return an instance of the "wrapper" class
+            @wrapper.new(document)
+          end
+        end
+      end
+
+      def __get_results_with_load(hits)
+        return [] if hits.empty?
+
+        records = {}
+        @response['hits']['hits'].group_by { |item| item['_type'] }.each do |type, items|
+          raise NoMethodError, "You have tried to eager load the model instances, " +
+                               "but Tire cannot find the model class because " +
+                               "document has no _type property." unless type
+
+          begin
+            klass = type.camelize.constantize
+          rescue NameError => e
+            raise NameError, "You have tried to eager load the model instances, but " +
+                             "Tire cannot find the model class '#{type.camelize}' " +
+                             "based on _type '#{type}'.", e.backtrace
+          end
+
+          records[type] = __find_records_by_ids klass, items.map { |h| h['_id'] }
+        end
+
+        # Reorder records to preserve the order from search results
+        @response['hits']['hits'].map do |item|
+          records[item['_type']].detect do |record|
+            record.id.to_s == item['_id'].to_s
+          end
+        end
+      end
+
+      def __find_records_by_ids(klass, ids)
+        @options[:load] === true ? klass.find(ids) : klass.find(ids, @options[:load])
       end
 
     end
