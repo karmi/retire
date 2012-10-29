3ed3cdce3da7682e80eac7b93299902f5e427f2b
Artyom Ignatiev <zazubrik@gmail.com>
2012-10-29 15:01:47 +0400

    failing testcase showing broken step-by-step query building
    Docs claim this should work:
    
    https://github.com/karmi/tire/blame/91bb6c7f2f8e4de9b7634f360f23fd0cb718
    47f4/README.markdown#L285

--- test/integration/dsl_search_test.rb
+++ test/integration/dsl_search_test.rb
@@ -15,6 +15,20 @@ module Tire
         assert_equal 2, s.results.facets['tags']['count']
       end
 
+      should "allow setting search payload later on" do
+        s = Tire.search 'articles-test'
+        s.query { string 'ruby' }
+        s.facet 'tags' do
+          filter :term, :tags => 'ruby'
+        end
+
+        # p s.to_hash
+        # p s.to_json
+        # p s.to_curl
+        assert_equal 2, s.results.count
+        assert_equal 2, s.results.facets['tags']['count']
+      end
+
     end
 
   end
