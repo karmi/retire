4d731319226e491772426cc53d60ef498fd52071
Karel Minarik <karmi@karmi.cz>
2012-11-08 21:28:31 +0100

    Release 0.5.0

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,15 +1,33 @@
 module Tire
-  VERSION   = "0.4.3"
+  VERSION   = "0.5.0"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    * Added a prefix query
-    * Added support for "script fields" (return a script evaluation for each hit)
-    * Added support for the Update API in Tire::Index
-    * [FIX] Fixed incorrect `Results::Item#to_hash` serialization
-    * 730813f Added support for aggregating over multiple fields in the "terms" facet
-    * Added the "Dis Max Query"
-    * Added the ability to transform documents when reindexing
+    * [!BREAKING!] Change format of sort/order in simple model searches to <field>:<direction>
+    * [FIX] Remove `page` and `per_page` from parameters sent to elasticsearch
+    * [FIX] Remove the `wrapper` options from URL params sent to elasticsearch
+    * [FIX] Use `options.delete(:sort)` in model search to halt bubbling of `sort` into URL parameters [#334]
+    * Added prettified JSON output for logging requests and responses at the `debug` level
+    * Improved the Rake import task
+    * Allow passing of arbitrary objects in the `:as` mapping option [#446]
+    * Allow to define default values for Tire::Model::Persistence `:as` lambdas
+    * Added `@search.results.max_score`
+    * Changed the URI escape/unescape compatibility patch to not require Rack
+    * Allow using the full DSL in filter facets
+    * Allow complex hash options for the `term` query
+    * Allow passing Hash-like objects to `terms` query as well
+    * Implemented `respond_to?` for `Item`
+    * Improved support for Kaminari pagination
+    * Added support for the `parent` URL parameter in `Index#store`
+    * Added the `min_score` and `track_scores` DSL methods
+    * Added support for loading partial fields
+    * Added support for boosting query
+    * Added the `facet_filter` DSL method
+    * Allow passing `routing`, `fields` and `preference` URL parameter to Index#retrieve
+    * Allow building the search request step-by-step in Tire's DSL [#496]
+    * Added a `match` query type
+    * Added support for multisearch (_msearch) and the `Tire.multi_search` DSL method
+    * Added support for multi-search in the ActiveModel integration and in Tire::Model::Persistence
   END
 end
