4cd01693987224c4ce48d3ea4fd6286fb59edf1b
Karel Minarik <karmi@karmi.cz>
2013-01-03 19:04:54 +0100

    Release 0.5.3

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,12 +1,11 @@
 module Tire
-  VERSION   = "0.5.2"
+  VERSION   = "0.5.3"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    * Fixed bugs in the `matches` Tire method in models
-    * Fixed JSON parser errors when logging empty bodies
-    * Allow arbitrary ordering of methods in the facet DSL block
-    * Fix issues with Mongoid gem (incorrect JSON serialization of Moped::BSON::ObjectId)
+    * Added `Tire::Results::Collection#each_with_hit`
+    * Added support for passing partial document to `Index#update`
+    * Added `nested` query support
   END
 end
