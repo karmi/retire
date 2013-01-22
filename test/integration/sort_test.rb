ee2ea2a0d8abfe43c6569200db60d002f4339a33
Karel Minarik <karmi@karmi.cz>
2013-01-22 12:04:47 +0100

    [TEST] Added integration test for sorting by multiple fields
    Closes #596

--- test/integration/sort_test.rb
+++ test/integration/sort_test.rb
@@ -29,6 +29,22 @@ module Tire
         assert_equal 'Two', s.results.first[:title]
       end
 
+      should "sort by multiple fields" do
+        q = '*'
+        s = Tire.search('articles-test') do
+          query { string q }
+          sort  do
+            by :words, :desc
+            by :title, :asc
+          end
+        end
+
+        # p s.results.to_a.map { |i| [i.title, i.sort] }
+        assert_equal 'Three', s.results[0][:title]
+        assert_equal 'Four',  s.results[1][:title]
+        assert_equal 'Two',   s.results[2][:title]
+      end
+
     end
 
   end
