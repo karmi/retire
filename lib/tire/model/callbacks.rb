ee8ae4446a638c6cd628d6c7f382d45b9e6ea303
Aaron Rustad <arustad@anassina.com>
2013-02-04 15:52:25 -0700

    Add the ability to limit index_updates to the attributes that are actually part of the index.

--- lib/tire/model/callbacks.rb
+++ lib/tire/model/callbacks.rb
@@ -1,3 +1,4 @@
+require 'set'
 module Tire
   module Model
 
@@ -11,6 +12,10 @@ module Tire
     #
     module Callbacks
 
+			def self.save_only_on_indexed
+				true
+			end
+
       # A hook triggered by the `include Tire::Model::Callbacks` statement in the model.
       #
       def self.included(base)
@@ -18,9 +23,22 @@ module Tire
         # Update index on model instance change or destroy.
         #
         if base.respond_to?(:after_save) && base.respond_to?(:after_destroy)
-          base.send :after_save,    lambda { tire.update_index }
+          base.send :after_save,    lambda { tire.update_index if should_save? }
           base.send :after_destroy, lambda { tire.update_index }
         end
+
+				def should_save?
+					if self.respond_to?(:changes) && self.class.save_only_on_index_changes?
+						unless defined? @indexed_properties
+							raise "No tire index mapping has been defined for #{self.class.name}" if self.tire.index.mapping.nil?	
+							@indexed_properties = Set.new(self.tire.index.mapping[self.class.name.underscore]['properties'].keys)
+						end	
+						changed_properties = self.changes.keys
+						changed_properties.any? {|changed_property| @indexed_properties.include? changed_property } 
+					else
+						return true
+					end
+				end
 
         # Add neccessary infrastructure for the model, when missing in
         # some half-baked ActiveModel implementations.
@@ -32,7 +50,18 @@ module Tire
           end
         end
 
+				base.extend(ClassMethods)
       end
+
+			module ClassMethods
+				def save_only_on_index_changes(should_change = false)
+					@save_only_on_index_changes = should_change
+				end
+
+				def save_only_on_index_changes?
+					@save_only_on_index_changes ||= false
+				end
+			end
 
     end
 
