8ce911c9517ec3279b3773d5c99658c12e7ddc3e
Jason Waldrip <jason@waldrip.net>
2013-03-01 16:26:40 -0700

    IMHO a better rake task.

--- lib/tire/tasks.rb
+++ lib/tire/tasks.rb
@@ -1,105 +1,115 @@
 require 'rake'
-require 'benchmark'
+require 'progress_bar'
+require 'set'
 
 namespace :tire do
 
   full_comment_import = <<-DESC.gsub(/    /, '')
     Import data from your model using paginate: rake environment tire:import CLASS='MyModel'.
 
-    Pass params for the `paginate` method:
-      $ rake environment tire:import CLASS='Article' PARAMS='{:page => 1}'
+    import all models:
+      $ rake tire:import
 
     Force rebuilding the index (delete and create):
-      $ rake environment tire:import CLASS='Article' PARAMS='{:page => 1}' FORCE=1
+      $ rake tire:import FORCE=1
 
-    Set target index name:
-      $ rake environment tire:import CLASS='Article' INDEX='articles-new'
+    Set target classes:
+      $ rake tire:import CLASS='Article, Comment'
   DESC
   desc full_comment_import
-  task :import do |t|
-
-    def elapsed_to_human(elapsed)
-      hour = 60*60
-      day  = hour*24
-
-      case elapsed
-      when 0..59
-        "#{sprintf("%1.2f", elapsed)} seconds"
-      when 60..hour-1
-        "#{(elapsed/60).floor} minutes and #{(elapsed % 60).floor} seconds"
-      when hour..day
-        "#{(elapsed/hour).floor} hours and #{(elapsed/60 % hour).floor} minutes"
-      else
-        "#{(elapsed/hour).round} hours"
-      end
-    end
-
-    if ENV['CLASS'].to_s == ''
-      puts '='*90, 'USAGE', '='*90, full_comment_import, ""
-      exit(1)
-    end
-
-    klass  = eval(ENV['CLASS'].to_s)
-    params = eval(ENV['PARAMS'].to_s) || {}
-
-    params.update :method => 'paginate'
-
-    index = Tire::Index.new( ENV['INDEX'] || klass.tire.index.name )
-
-    if ENV['FORCE']
-      puts "[IMPORT] Deleting index '#{index.name}'"
-      index.delete
-    end
-
-    unless index.exists?
-      mapping = MultiJson.encode(klass.tire.mapping_to_hash, :pretty => Tire::Configuration.pretty)
-      puts "[IMPORT] Creating index '#{index.name}' with mapping:", mapping
-      unless index.create( :mappings => klass.tire.mapping_to_hash, :settings => klass.tire.settings )
-        STDERR.puts "[ERROR] There has been an error when creating the index -- elasticsearch returned:",
-                    index.response
-        exit(1)
-      end
-    end
+  task :import => :environment do
 
     STDOUT.sync = true
-    puts "[IMPORT] Starting import for the '#{ENV['CLASS']}' class"
-    tty_cols = 80
-    total    = klass.count rescue nil
-    offset   = (total.to_s.size*2)+8
-    done     = 0
+    TIRE_MODELS = Set.new
+    HRULE       = '='*90
 
-    STDOUT.puts '-'*tty_cols
-    elapsed = Benchmark.realtime do
+    if ENV['CLASS'].to_s != ''
+      # Use Models in the ENV Vars
+      TIRE_MODELS += ENV['CLASS'].split(',').map { |k| eval k }
 
-      # Add Kaminari-powered "paginate" method
-      #
-      if defined?(Kaminari) && klass.respond_to?(:page)
-        klass.instance_eval do
-          def paginate(options = {})
-            page(options[:page]).per(options[:per_page]).to_a
-          end
+    else
+      # Find all the classes which call mapping
+      puts "\n", "[IMPORT] Preloading Models..."
+      Tire::Model::Search::ClassMethods.module_eval do
+        def mapping(*args, &block)
+          TIRE_MODELS << self.klass
+          super(*args, &block)
         end
-      end unless klass.respond_to?(:paginate)
+      end
 
-      # Import the documents
-      #
-      index.import(klass, params) do |documents|
+      # Require everything in the models directory
+      Dir.glob(Rails.root.join('app/models/**/*.rb')).each { |path| require path }
+    end
 
-        if total
-          done += documents.to_a.size
-          # I CAN HAZ PROGREZ BAR LIEK HOMEBRU!
-          percent  = ( (done.to_f / total) * 100 ).to_i
-          glyphs   = ( percent * ( (tty_cols-offset).to_f/100 ) ).to_i
-          STDOUT.print( "#" * glyphs )
-          STDOUT.print( "\r"*tty_cols+"#{done}/#{total} | \e[1m#{percent}%\e[0m " )
-        end
+    # exit(1) if no models are found!
+    puts HRULE, 'USAGE', HRULE, full_comment_import, "" and exit(1) unless TIRE_MODELS.size > 0
 
-        # Don't forget to return the documents collection back!
-        documents
+    # Import Methods
+    TOTAL_COUNT = TIRE_MODELS.map(&:count).reduce(&:+)
+
+    def params
+      @params ||= begin
+        params = eval(ENV['PARAMS'].to_s) || {}
+        params.update :method => 'paginate'
       end
     end
 
-    puts "", '='*80, "Import finished in #{elapsed_to_human(elapsed)}"
+    TIRE_MODELS.each do |klass|
+
+      puts "\n\n"
+
+      index = klass.tire.index
+
+      # Force delete the index
+      if ENV['FORCE']
+        puts "[IMPORT] Deleting index '#{index.name}'"
+        index.delete
+      end
+
+      # Create the index if it doesn't exist
+      unless index.exists?
+        mapping = MultiJson.encode(klass.tire.mapping_to_hash, :pretty => Tire::Configuration.pretty)
+        puts "[IMPORT] Creating index '#{index.name}' with mapping:", mapping
+        unless index.create(:mappings => klass.tire.mapping_to_hash, :settings => klass.tire.settings)
+          STDERR.puts "[ERROR] There has been an error when creating the index -- elasticsearch returned:",
+                      index.response
+          exit(1)
+        end
+      end
+
+      puts "[IMPORT] Starting import for the '#{klass}' class"
+      progress_bar = ProgressBar.new(klass.count)
+
+      # Use an indexer scope if it is defined
+      klass = klass.indexer if klass.respond_to? :indexer
+
+      # Try and use AR find_in_batches
+      if klass.respond_to?(:find_in_batches)
+        klass.find_in_batches do |group|
+          index.import(group, params) do |documents|
+            GC.start
+            progress_bar.increment! documents.count
+            documents
+          end
+        end
+      elsif klass.respond_to? :all
+        index.import(klass.all, params) do |documents|
+          GC.start
+          progress_bar.increment! documents.count
+          documents
+        end
+      else
+        index.import(klass, params) do |documents|
+          GC.start
+          progress_bar.increment! documents.count
+          documents
+        end
+      end
+
+    end
+
+    puts '[DONE]'
+
   end
 
   namespace :index do
@@ -126,7 +136,7 @@ namespace :tire do
       index_names.each do |name|
         index = Tire::Index.new(name)
         print "* Deleting index \e[1m#{index.name}\e[0m... "
-        puts  index.delete ? "\e[32mOK\e[0m" : "\e[31mFAILED\e[0m  | #{index.response.body}"
+        puts index.delete ? "\e[32mOK\e[0m" : "\e[31mFAILED\e[0m  | #{index.response.body}"
       end
 
       puts ""
