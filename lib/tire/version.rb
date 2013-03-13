f185eda7f7e37090085aab0cc526b65f4067ad72
Karel Minarik <karmi@karmi.cz>
2013-03-13 09:52:15 +0100

    Release 0.5.5

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,18 +1,14 @@
 module Tire
-  VERSION   = "0.5.4"
+  VERSION   = "0.5.5"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    * Added the support for the Count API
-    * Escape single quotes in `to_curl` serialization
-    * Added JRuby compatibility
-    * Added proper `as_json` support for `Results::Collection` and `Results::Item` classes
-    * Added extracting the `routing` information in the `Index#store` method
-    * Refactored the `update_index` method for search and persistence integration
-    * Cast collection properties in Model::Persistence as empty Array by default
-    * Allow passing `:index` option to `MyModel.import`
-    * Update to Mocha ~> 0.13
-    * Update to MultiJson ~> 1.3
+    * Improved documentation
+    * Improved isolation of Tire methods in model integrations
+    * Improved handling of times/dates in `Model::Persistence`
+    * Added support for "Put Mapping" and "Delete mapping" APIs
+    * Added escaping document IDs in URLs
+    * Allowed passing URL options when passing search definition as a Hash
   END
 end
