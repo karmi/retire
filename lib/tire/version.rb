c35028539c33e1be967ebe228cff22fb754e7bab
Karel Minarik <karmi@karmi.cz>
2013-03-29 09:46:08 +0100

    Release 0.5.7

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,5 +1,5 @@
 module Tire
-  VERSION   = "0.5.6"
+  VERSION   = "0.5.7"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
@@ -7,5 +7,6 @@ module Tire
     * Added support for the `constant_score` query
     * Prevent `Curl::Err::MultiBadEasyHandle` errors in the Curb client
     * Refactored the model importing integration and Rake tasks
+    * Enabled passing options to the `terms` query
   END
 end
