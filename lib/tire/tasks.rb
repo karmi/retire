9484a836692f976aa9e8826539728601f548172e
Karel Minarik <karmi@karmi.cz>
2013-06-05 13:41:34 +0200

    Removed the auto-loading of Rails' `environment` task
    As reported by some users, the auto-loading didn't invoked/executed the task properly,
    resulting in errors eg. due to not loaded initializers.
    
    The auto-invoking has been removed, since the documentation clearly states
    to invoke the task before the importing one.
    
    Closes #708

--- lib/tire/tasks.rb
+++ lib/tire/tasks.rb
@@ -87,11 +87,6 @@ namespace :tire do
   namespace :import do
     desc import_model_desc
     task :model do
-      if defined?(Rails)
-        puts "[IMPORT] Rails detected, loading environment..."
-        Rake::Task["environment"].invoke
-      end
-
       if ENV['CLASS'].to_s == ''
         puts '='*90, 'USAGE', '='*90, import_model_desc, ""
         exit(1)
@@ -121,11 +116,6 @@ namespace :tire do
 
     desc import_all_desc
     task :all do
-      if defined?(Rails)
-        puts "[IMPORT] Rails detected, loading environment..."
-        Rake::Task["environment"].invoke
-      end
-
       dir    = ENV['DIR'].to_s != '' ? ENV['DIR'] : Rails.root.join("app/models")
       params = eval(ENV['PARAMS'].to_s) || {}
 
