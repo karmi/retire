3c8c6991800cdd870e44f8cc567e2fba2637d3ee
Karel Minarik <karmi@karmi.cz>
2013-06-08 17:15:41 +0200

    Release v0.6.0

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,14 +1,20 @@
 module Tire
-  VERSION   = "0.5.8"
+  VERSION   = "0.6.0"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    * Fixed, that Model::Persistence uses "string" as the default mapping type
-    * Fixed, that Model::Persistence returns true/false for #save and #destroy operations
-    * Fixed the `uninitialized constant HRULE` in Rake tasks
-    * Fixed `Item#to_hash` functionality to work with Arrays
-    * Updated the Rails application template and install instructions
-    * Improved the test suite for Travis
+    * Fixed incorrect inflection in the Rake import tasks
+    * Added support for `geo_distance` facets
+    * Added support for the `custom_filters_score` query
+    * Added a custom strategy option to <Model.import>
+    * Allow the `:wrapper` option to be passed to Tire.search consistently
+    * Improved the Mongoid importing strategy
+    * Merge returned `fields` with `_source` if both are returned
+    * Removed the deprecated `text` query
+    * [FIX] Rescue HTTP client specific connection errors in MyModel#create_elasticsearch_index
+    * Added support for passing `version` in Tire::Index#store
+    * Added support for `_version_type` in Tire::Index#bulk
+    * Added ActiveModel::Serializers compatibility
   END
 end
