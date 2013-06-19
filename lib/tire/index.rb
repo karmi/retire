c4cd96651a365396e5939e558c2f9ca0200ac39a
Philip Stevens <philip.stevens@reevoo.com>
2013-06-19 14:57:32 +0100

    Use Configuration.url

--- lib/tire/index.rb
+++ lib/tire/index.rb
@@ -120,13 +120,16 @@ module Tire
     end
 
     def nodes(options={})
-      url = "#{self.url}/_nodes"
+      url = "#{Configuration.url}/_nodes"
       unless options.empty?
         params = options.map { |k, v| "#{k}=#{v}" }
         url += "?#{params.join('&')}" if params
       end
       @response = Configuration.client.get(url)
       MultiJson.decode(@response.body)['nodes'].values
+    ensure
+      curl = %Q|curl -X GET "#{url}"|
+      logged('GET NODES', curl)
     end
 
     def store(*args)
