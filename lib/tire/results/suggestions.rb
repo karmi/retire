34a1c8aed25b979723197555878801b151955b98
Fábio <fbatista@webreakstuff.com>
2013-08-29 18:54:33 +0100

    Added suggest support to the model integration
    Related: #916, #857

--- lib/tire/results/suggestions.rb
+++ lib/tire/results/suggestions.rb
@@ -7,11 +7,61 @@ module Tire
   		def initialize(response, options={})
         @response = response
         @options = options
+        @shards_info ||= @response.delete '_shards'
+        @keys ||= @response.keys
       end
 
       def results
         return [] if failure?
         @results ||= @response
+      end
+
+      def keys
+        @keys
+      end
+
+      def each(&block)
+        results.each(&block)
+      end
+
+      def size
+        results.size
+      end
+
+      def options(suggestion=:all)
+        if suggestion == :all
+          results.map {|k,v| v.map{|s| s['options']}}.flatten
+        else
+          response[suggestion].map{|s| s['options']}.flatten
+        end
+      end
+
+      def texts(suggestion=:all)
+        if suggestion == :all
+          results.map {|k,v| v.map{|s| s['options'].map {|o| o['text']}}}.flatten
+        else
+          response[suggestion].map{|s| s['options'].map {|o| o['text']}}.flatten
+        end
+      end
+
+      def payloads(suggestion=:all)
+        if suggestion == :all
+          results.map {|k,v| v.map{|s| s['options'].map {|o| o['payload']}}}.flatten
+        else
+          response[suggestion].map{|s| s['options'].map {|o| o['payload']}}.flatten
+        end
+      end
+
+      def error
+        @response['error']
+      end
+
+      def success?
+        error.to_s.empty?
+      end
+
+      def failure?
+        ! success?
       end
   	end
 	end
