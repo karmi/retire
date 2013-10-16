c668c9b9d58b28920f18df2f2c85a9aa716c8bc4
Karel Minarik <karmi@karmi.cz>
2013-10-16 09:32:13 +0200

    Release 0.6.1

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,20 +1,15 @@
 module Tire
-  VERSION   = "0.6.0"
+  VERSION   = "0.6.1"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
-    * Fixed incorrect inflection in the Rake import tasks
-    * Added support for `geo_distance` facets
-    * Added support for the `custom_filters_score` query
-    * Added a custom strategy option to <Model.import>
-    * Allow the `:wrapper` option to be passed to Tire.search consistently
-    * Improved the Mongoid importing strategy
-    * Merge returned `fields` with `_source` if both are returned
-    * Removed the deprecated `text` query
-    * [FIX] Rescue HTTP client specific connection errors in MyModel#create_elasticsearch_index
-    * Added support for passing `version` in Tire::Index#store
-    * Added support for `_version_type` in Tire::Index#bulk
-    * Added ActiveModel::Serializers compatibility
+    * Added support for bulk update
+    * Improved Kaminari support
+    * Improved the behaviour of `default` properties in Tire::Persistence
+    * Added the information about the gem "retirement" and other documentation improvements
+    * Fixed errors due to NewRelic's patching of Curl
+    * [ACTIVEMODEL] Use Object#id#to_s in `get_id_from_document`
+    * Added support for "Delete By Query" API
   END
 end
