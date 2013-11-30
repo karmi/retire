75ad9c0018645b37abfe0f166ce8cddd7cc030bc
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-11-30 15:37:09 +0100

    Suggestions: Working integration test within search

--- test/integration/suggest_test.rb
+++ test/integration/suggest_test.rb
@@ -5,31 +5,33 @@ module Tire
   class SuggestIntegrationTest < Test::Unit::TestCase
     include Test::Integration
 
-    context "Suggest" do
+    context "Search Suggest" do
 
       should "add suggestions field to the results using the term suggester" do
         # Tire::Configuration.logger STDERR, :level => 'debug'
         s = Tire.search('articles-test') do
-          suggest :term_suggest1, 'thrree' do
+          suggest :term_suggest1 do
+            text 'thrree'
             term :title
           end
         end
 
         assert_equal 1, s.results.suggestions.size
-        assert_equal 'three', s.results.suggestions["term_suggest1"].first["options"].first["text"]
+        assert_equal 'three', s.results.suggestions.texts.first
       end
     end
 
     should "add suggestions field to the results using the phrase suggester" do
         # Tire::Configuration.logger STDERR, :level => 'debug'
         s = Tire.search('articles-test') do
-          suggest :term_suggest1, 'thrree' do
+          suggest :term_suggest1 do
+            text 'thrree'
             phrase :title
           end
         end
 
         assert_equal 1, s.results.suggestions.size
-        assert_equal 'three', s.results.suggestions["term_suggest1"].first["options"].first["text"]
+        assert_equal 'three', s.results.suggestions.texts.first
     end
   end
 end
